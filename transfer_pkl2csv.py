915ac5148a62386b5b97ee03fc4fcf7c6df4f752
ffding <dff@pku.edu.cn>
2023-03-02 08:33:24 +0000

    update 202303

--- transfer_pkl2csv.py
+++ transfer_pkl2csv.py
@@ -5,11 +5,11 @@ def load_dic(name):
     with open(name, 'rb') as f:
         return pickle.load(f)
 
-csvFile=open("data/recommend_202211.csv",'w',newline='')
+csvFile=open("data/recommend_202302.csv",'w',newline='')
 writer=csv.writer(csvFile)
 writer.writerow(("user_id", "ckey_list"))
 
-data = load_dic("data/recommend_202211.pkl")
+data = load_dic("data/recommend_202302.pkl")
 
 for user_id, recommend in data.items():
     writer.writerow((user_id, recommend["ckey_list"]))
